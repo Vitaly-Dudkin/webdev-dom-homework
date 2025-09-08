6704466f07f59bd72c5f42cd261ca504c29a75b4
Виталий <dudkin231@mail.ru>
2025-09-08 14:17:32 +0300

    added some chamges

--- modules/script.js
+++ modules/script.js
@@ -5,26 +5,7 @@ import { addButton, nameElement, textElement } from './vars.js'
 
 loadComments()
 
-addButton.addEventListener('click', () => {
-    let nameUser = nameElement.value
-    let text = textElement.value
-
-    addComment() // Добавляем комментарий
-    nameUser = '' // Очищаем поля ввода
-    text = ''
-})
-
 addButton.addEventListener('click', addComment)
 
 nameElement.addEventListener('input', updateButtonState)
 textElement.addEventListener('input', updateButtonState)
-
-textElement.addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault() // предотвращаем переход на новую строку
-        if (!addButton.disabled) {
-            // Проверяем, не отключена ли кнопка
-            addComment()
-        }
-    }
-})
